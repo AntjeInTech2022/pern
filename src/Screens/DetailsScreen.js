602b328ffe716fc3bee733cf9a6983719aeff5fd
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-11-07 15:54:15 +0100

    npm i react-router-dom npm i @types/react-router-dom

--- src/Screens/DetailsScreen.js
+++ src/Screens/DetailsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function DetailsScreen() {
   return <div>DetailsScreen</div>;
--- src/Screens/LandingScreen.js
+++ src/Screens/LandingScreen.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
 
 function LandingScreen() {
-  return <div>LandingScreen</div>;
+  return (
+    <div class="banner">
+      <div class="container">
+        <h1>
+          <strong>Looking for a place to bee</strong>?
+        </h1>
+        <p>
+          You have space for bees to thrive but no hive?
+          <br />
+          Got bees but nowhere to be?
+        </p>
+        <p>BUZZ brings you together</p>
+        <div>
+          {/* <%= link_to "Fields to rent", 'fields', class: "btn btn-warning" %>
+      <%= link_to "Rent your field", 'fields/new', class: "btn btn-warning" %> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LandingScreen;

78486363213fbc713c4de0ee169915024a755106
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-11-09 11:09:10 +0100

    cleaning image files

--- client/src/Screens/LandingScreen.js
+++ client/src/Screens/LandingScreen.js
@@ -1,40 +1,15 @@
 import React from 'react';
 
-//MUI
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { Box } from '@mui/system';
-
-//IMAGES
-import card_picture from '../Images/pexels-anete-lusina-5247969.jpg';
-
 //COMPONENTS
-import Cards from '../components/Cards';
+
 import ProductHero from '../components/c_landingPage/ProductHero';
 import HowItWorks from '../components/c_landingPage/HowItWorks';
 
 function LandingScreen() {
   return (
     <>
-      {/* <Container maxWidth="sm"> */}
       <ProductHero />
       <HowItWorks />
-      {/* <Container sx={{ width: 900 }}>
-        <Box marginTop={3} sx={{ display: 'flex' }}>
-          <img src={card_picture} height={325} />
-        </Box>
-        <Typography variant="h2" gutterBottom marginTop={3}>
-          Looking for a place to bee?
-        </Typography>
-        <Typography variant="h5" gutterBottom>
-          You have space for bees to thrive but no hive?
-          <br />
-          Got bees but nowhere to be?
-          <br /> BuZz brings you together.
-        </Typography>
-      </Container> */}
     </>
   );
 }

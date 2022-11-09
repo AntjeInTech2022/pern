be5bde6b54072ca38762a323dbc06df1bc66153a
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-11-09 15:17:59 +0100

    sign-Up form+screen created

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,26 @@ import './App.css';
 import LandingScreen from './Screens/LandingScreen';
 import ListScreen from './Screens/ListScreen';
 import DetailsScreen from './Screens/DetailsScreen';
-import RegisterScreen from './Screens/RegisterScreen';
+
 import ProfileScreen from './Screens/ProfileScreen';
 
 // COMPONENTS
-import NavAppBar from './components/c_navigation/AppBar';
+
 import AppAppBar from './components/c_navigation/AppAppBar';
 import AppFooter from './components/c_footer/AppFooter';
+import SignUp from './Screens/SignUp';
 
 function App() {
   return (
     <div className="App">
-      {/* <NavAppBar /> */}
       <AppAppBar />
       <Router>
         <Routes>
           <Route path="/" element={<LandingScreen />}></Route>
           <Route path="/list" element={<ListScreen />}></Route>
           <Route path="/details/:id" element={<DetailsScreen />}></Route>
-          <Route path="/register" element={<RegisterScreen />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          {/* <Route path="/login" element={<Login />}></Route> */}
           <Route path="/profile" element={<ProfileScreen />}></Route>
         </Routes>
         <AppFooter />

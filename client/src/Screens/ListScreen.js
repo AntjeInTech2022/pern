75cc1c45e4393be76882999b032db87c7c0fedca
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-11-10 11:51:19 +0100

    fixed card display row in listScreen.js

--- client/src/Screens/ListScreen.js
+++ client/src/Screens/ListScreen.js
@@ -3,9 +3,41 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 //COMPONENTS
-import HostCards from '../components/Cards';
+import HostCard from '../components/Cards';
+
+//DUMMY HOST DATA
+const data = [
+  {
+    src: 'https://images.unsplash.com/photo-1563281709-7af174759d98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80',
+    alt: 'https://unsplash.com/photos/z8gKIE4Kz0Y',
+    host_name: 'Gunda',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricie',
+    location: 'Brandenburg',
+    createdAt: 'a week ago',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1582374927372-b06cb28a3bc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    alt: 'https://unsplash.com/photos/fgzDK9ga43M',
+    host_name: 'Daniela',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricie',
+    location: 'Pfalz',
+    createdAt: '3 years ago',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1463319492205-164b553bfae5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    alt: 'https://unsplash.com/photos/OnKIsDLCeZ8',
+    host_name: 'Markus',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricie',
+    location: 'Brandenburg',
+    createdAt: '10 months ago',
+  },
+];
 
 function ListScreen() {
   return (
@@ -13,19 +45,12 @@ function ListScreen() {
       <Typography variant="h2" gutterBottom marginTop={3} marginBottom={5}>
         Featured Hosts
       </Typography>
-      <Grid
-        container
-        // spacing={4}
-        // direction="row"
-        // columns={16}
-        //rowSpacing={1}
-        //columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      >
-        {/* <Grid item xs={12} sm={6} md={4}> */}
-        {/* <Grid item> */}
-        <HostCards />
-        {/* <BeeKeeper_Cards /> */}
-        {/* </Grid> */}
+      <Grid container spacing={4}>
+        {data.map((item, index) => (
+          <Grid item key={index} xs={12} md={3} lg={4}>
+            <HostCard item={item} />
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );

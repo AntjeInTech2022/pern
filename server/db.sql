c39d44647aa0230b87433afecf3ff7e80bca4099
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-11-11 14:06:40 +0100

    add new user to table users_hosts

--- server/db.sql
+++ server/db.sql
@@ -3,3 +3,8 @@ CREATE TABLE users_hosts(
     pid SERIAL PRIMARY KEY,
     user_name VARCHAR(255)
 );
+
+-- test=# 
+INSERT INTO users_hosts(user_name) VALUES ('Antje'); -- will automatically generate a new pid
+SELECT * FROM users_hosts;
+UPDATE users_hosts SET user_name = ('Chris')  WHERE pid = ('3'); 

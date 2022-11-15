3121122d76348fddac564de7d0bd2c2ed0234daa
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-11-15 11:35:13 +0100

    import jwt fr "jsonwebtoken" bcrypt from "bcrypt"

--- server/controller/testController.js
+++ server/controller/testController.js
@@ -6,20 +6,34 @@ import pool from "../dbConfig.js";
 // Registration
 
 const Register = async (req, res) => {
+  // 1.  destructure the req.body (name,email, password)
+  const { email, name, password } = req.body;
   try {
-    // 1.  destructure the req.body (name,email, password)
-    const { name, email, password } = req.body;
     // 2. check if user exists (if user exists throw error)
     const user = await pool.query("SELECT * FROM users WHERE user_email =$1", [
       email,
     ]);
     // if user exists throw error:
     if (user.rows.lenght !== 0) {
-      return res.status(401);
+      return res.status(401).send("User already exists");
     }
     // 3. bcrypt the user password
-    // 4. add user to database table 'users'
+    // npm i bcrypt (https://www.npmjs.com/package/bcrypt)
+    const bcrypt = require("bcrypt");
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+    // 4. add new user to database table 'users'
+    const newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
+
+    res.json(newUser.rows[0]);
+
     // 5. generate jwt token
+    // npm install dotenv
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server error");
---
+++ server/utils/jwtGenerator.js
@@ -0,0 +1,2 @@
+// npm i jsonwebtoken
+const jwt = require("jsonwebtoken");
